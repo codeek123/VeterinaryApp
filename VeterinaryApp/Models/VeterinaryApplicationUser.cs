f4225fd5f2852a76a8bc48bf16c65b947feda36c
codeek123 <eva.koceva12@gmail.com>
2025-03-07 15:00:55 +0100

    Identity User

---
+++ VeterinaryApp/Models/VeterinaryApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VeterinaryApp.Models
+{
+
+    public class VeterinaryApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
