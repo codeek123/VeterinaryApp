b1dbacca97bab32a673bfc75f0b82cc8a740dd98
codeek123 <eva.koceva12@gmail.com>
2025-03-07 21:56:08 +0100

    add Identity User

--- VeterinaryApp/Data/ApplicationDbContext.cs
+++ VeterinaryApp/Data/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using VeterinaryApp.Models;
 
 namespace VeterinaryApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<VeterinaryAppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Vaccine> Vaccines { get; set; }
     }
 }
---
+++ VeterinaryApp/Models/VeterinaryAppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VeterinaryApp.Models
+{
+    public class VeterinaryAppUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
