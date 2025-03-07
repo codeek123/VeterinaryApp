77bd3fd72c7537b09f71f3862ec906b2539f23fd
ivagj <gjorgievska.ivana@yahoo.com>
2025-03-07 14:30:57 +0100

    add-model

--- VeterinaryApp/Models/Owner.cs
+++ VeterinaryApp/Models/Owner.cs
@@ -1,6 +1,20 @@
-﻿namespace VeterinaryApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryApp.Models
 {
     public class Owner
     {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Range(18, 100, ErrorMessage = "Age must be between 18 and 100.")]
+        public int Age { get; set; }
+
+      //  public ICollection<Pet> Pets { get; set; } 
+        
+        public string GetFullName() => $"{Name} {Surname}";
     }
 }
---
+++ VeterinaryApp/Models/PetVaccine.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace VeterinaryApp.Models
+{
+    public class PetVaccine
+    {
+        public int PetId { get; set; }
+       // public Pet Pet { get; set; }
+
+       // public int VaccineId { get; set; }
+       // public Vaccine Vaccine { get; set; }
+    }
+}
