7610dbde0c93dea62d554afd5e7fce4b27e1cf5b
ivagj <gjorgievska.ivana@yahoo.com>
2025-03-07 14:37:47 +0100

    adding

--- VeterinaryApp/Models/Owner.cs
+++ VeterinaryApp/Models/Owner.cs
@@ -1,6 +1,21 @@
-﻿namespace VeterinaryApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryApp.Models
 {
-    public class Owner
+    public class Owner : BaseEntity
     {
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Range(18, 100, ErrorMessage = "Age must be between 18 and 100.")]
+        public int Age { get; set; }
+
+        public List<Pet> Pets { get; set; } = new();
+
+        public string GetFullName() => $"{Name} {Surname}";
     }
 }
--- VeterinaryApp/Models/Pet.cs
+++ VeterinaryApp/Models/Pet.cs
@@ -8,8 +8,8 @@ namespace VeterinaryApp.Models
         public string Name { get; set; }
         [Range(0, 50, ErrorMessage = "Age must be between 0 and 50 years.")]
         public int Age { get; set; }
-       // public int OwnerId { get; set; }
-       //public Owner Owner { get; set; }
+        public int OwnerId { get; set; }
+       public Owner Owner { get; set; }
 
         public ICollection<Vaccine> Vaccines { get; set; }
 
